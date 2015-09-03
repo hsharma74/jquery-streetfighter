42a4c2583d54b27e621e99e2838f980171a4921e
hsharma1974 <himanshu6312@hotmail.com>
2015-09-03 05:55:52 -0700

    All changes in. Sound not working.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,39 @@
-$('ryu-position').click(function() {
-	$().hide();
-	
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {	
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		// play hadouken sound
+		//playHadouken();
+		// show hadouken and animate it to the right
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		//$('.hadouken').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'}, 500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '380px');
+			}
+		);
+	})
+	.mouseup(function() {
+		// ryu goes back to his ready position
+		$('.ryu-throwing').hide();
+		$('.hadouken').hide();
+		$('.ryu-ready').show();
+	});
+
+
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
